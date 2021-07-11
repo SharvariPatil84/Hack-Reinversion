522aee287803acddb7bcc8119d14ade5466b8b56
Devang Kamble <risingentropy20@gmail.com>
2021-07-11 16:32:54 +0530

    qwertyuio

--- Healife/settings.py
+++ Healife/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6o8z%$9mu*&00^e@t(4!oy)^sy5ypqz+b-xk)0+g9cb4-zri_4'
+a = 'django-insecure-6o8z%$9mu*&00^e@t(4!'
+SECRET_KEY = a+'oy)^sy5ypqz+b-xk)0+g9cb4-zri_4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -42,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -77,8 +81,12 @@ WSGI_APPLICATION = 'Healife.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogsite',
+        'HOST': 'postgresql-38025-0.cloudclusters.net',
+        'USER': 'sharvari',
+        'PASSWORD': 'Admin@123',
+        'PORT': '38025'
     }
 }
 
@@ -124,6 +132,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn WCECourses.wsgi --log-file -
